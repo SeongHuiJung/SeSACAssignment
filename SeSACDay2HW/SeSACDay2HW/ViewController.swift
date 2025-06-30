f4c83f95f8f7d5ad7127301479b90c9e6e904852
SeongHuiJung <tjdgml150237@naver.com>
2025-06-30 18:02:24 +0900

    초기 설정

---
+++ SeSACDay2HW/SeSACDay2HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACDay2HW
+//
+//  Created by 정성희 on 6/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
