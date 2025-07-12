66c9e899d923a883112b24b6dc55db3ed4417d38
SeongHuiJung <tjdgml150237@naver.com>
2025-07-12 12:23:55 +0900

    [Setting] [Day11] 프로젝트 세팅

---
+++ SeSACDay11HW/SeSACDay11HW/Magazine/MagazineFeed.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SeSACDay11HW
+//
+//  Created by 정성희 on 7/11/25.
+//
+
+import UIKit
+
+class MagazineFeed: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
