3561f9fc287a7a4b329cfbbb6134ec209a831756
SeongHuiJung <tjdgml150237@naver.com>
2025-07-13 23:49:10 +0900

    [Comment] [Day11] 주석 제거

--- SeSACDay11HW/SeSACDay11HW/Travel/TravelModel.swift
+++ SeSACDay11HW/SeSACDay11HW/Travel/TravelModel.swift
@@ -11,7 +11,7 @@ struct Travel {
     var title: String
     var description: String?
     var travel_image: String?
-    var grade: Double? // star
+    var grade: Double?
     var save: Int?
     var like: Bool?
     var ad: Bool
