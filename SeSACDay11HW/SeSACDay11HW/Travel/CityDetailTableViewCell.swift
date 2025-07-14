c99ce5914cade3594f1731c47dd0d0b9568dd92b
SeongHuiJung <tjdgml150237@naver.com>
2025-07-14 21:09:40 +0900

    [Refactor] [Day12] Day11 과제 리팩토링 : King Fisher DownSampling 적용

--- SeSACDay11HW/SeSACDay11HW/Travel/CityDetailTableViewCell.swift
+++ SeSACDay11HW/SeSACDay11HW/Travel/CityDetailTableViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class CityDetailTableViewCell: UITableViewCell {
     
@@ -75,8 +76,16 @@ class CityDetailTableViewCell: UITableViewCell {
         }
         
         if let url = URL(string: row.travel_image ?? "") {
-            
-            travelImage.kf.setImage(with: url)
+            // Down Sampling
+            let processor = DownsamplingImageProcessor(size: travelImage.bounds.size)
+            travelImage.kf.setImage(with: url,
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .cacheOriginalImage, // 캐시가 있으면 캐시를 불러오고, 캐시가 없다면 네트워킹으로 데이터를 새로 받아옴
+                .memoryCacheExpiration(.days(1)), // 메모리 캐시 유지 기간을 1일로 설정
+                .diskCacheExpiration(.never) // 디스크 캐시는 만료되지 않도록 설정
+            ])
         }
         else {
             travelImage.image = UIImage(systemName: "xmark.diamond.fill")
