96311040ed499b372fb374d9ef345e34c7daa399
SeongHuiJung <tjdgml150237@naver.com>
2025-07-18 21:29:00 +0900

    [Add] [Day16] 다이렉트 메시지 프로젝트 생성

---
+++ DirectMessage/DirectMessage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DirectMessage
+//
+//  Created by 정성희 on 7/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
