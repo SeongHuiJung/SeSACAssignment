1db2531eeeba8c047685eb3debe0552a45105a94
SeongHuiJung <tjdgml150237@naver.com>
2025-07-19 16:03:16 +0900

    [Add] [Day16-1] 프로젝트 구조 세팅

---
+++ DirectMessage/DirectMessage/TalkList/View/TalkListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TalkListCollectionViewCell.swift
+//  DirectMessage
+//
+//  Created by 정성희 on 7/19/25.
+//
+
+import UIKit
+
+class TalkListCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
