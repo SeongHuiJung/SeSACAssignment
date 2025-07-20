da7c1085e85d14747b69aac287ae082dbf165e35
SeongHuiJung <tjdgml150237@naver.com>
2025-07-20 17:10:34 +0900

    [Feat] [Day16-2] 텍스트뷰 3줄까지 늘리는 기능 구현

--- DirectMessage/DirectMessage/ChattingRoom/Controller/ChattingRoomViewController.swift
+++ DirectMessage/DirectMessage/ChattingRoom/Controller/ChattingRoomViewController.swift
@@ -11,8 +11,9 @@ class ChattingRoomViewController: UIViewController {
 
     static let identifier = "ChattingRoomViewController"
     
+    @IBOutlet var textView: UITextView!
     @IBOutlet var tableView: UITableView!
-    @IBOutlet var textField: UITextField!
+    @IBOutlet var sendChatButton: UIButton!
     
     var chatData = ChatRoom(chatroomId: 0, chatroomImage: "", chatroomName: "")
     
@@ -22,16 +23,12 @@ class ChattingRoomViewController: UIViewController {
     }
 }
 
-// MARK: - Logic
+// MARK: - Action
 extension ChattingRoomViewController {
-    private func showLastChat() {
-        let lastIndex = IndexPath(row: chatData.chatList.count - 1, section: 0)
-        tableView.scrollToRow(at: lastIndex, at: .bottom, animated: false)
-    }
-    
-    @IBAction func textFieldDidEndOnExit(_ sender: UITextField) {
-        guard let text = textField.text else { return }
+    @IBAction private func sendChatButtonTapped(_ sender: UIButton) {
+        guard let text = textView.text else { return }
         guard !text.isEmpty else { return }
+        guard !isOnlyWhiteSpace(text: text) else { return }
         
         let chatRoomId = chatData.chatroomId
         
@@ -45,11 +42,59 @@ extension ChattingRoomViewController {
         
         fetchData(chatRoomId: chatRoomId)
         showLastChat()
+        textView.text = ""
+        resetTextViewSize()
+    }
+}
+
+// MARK: - Logic
+extension ChattingRoomViewController {
+    private func showLastChat() {
+        let lastIndex = IndexPath(row: chatData.chatList.count - 1, section: 0)
+        tableView.scrollToRow(at: lastIndex, at: .bottom, animated: false)
     }
     
     private func fetchData(chatRoomId: Int) {
         chatData = ChatList.list[chatRoomId - 1]
         tableView.reloadData()
+    }
+    
+    private func resetTextViewSize() {
+        let size = CGSize(width: view.frame.width, height: .infinity)
+        let estimatedSize = textView.sizeThatFits(size)
+        
+        textView.constraints.forEach { (constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = estimatedSize.height
+            }
+        }
+    }
+    
+    private func isOnlyWhiteSpace(text: String) -> Bool {
+        for char in text {
+            if char != "\n" && char != " " {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+// MARK: - TextView Delegate
+extension ChattingRoomViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        guard let lineHeight = textView.font?.lineHeight else { return }
+        
+        let size = CGSize(width: view.frame.width, height: .infinity)
+        let estimatedSize = textView.sizeThatFits(size)
+
+        textView.constraints.forEach { (constraint) in
+            if estimatedSize.height <= 16.6 + lineHeight * 3 {
+                if constraint.firstAttribute == .height {
+                    constraint.constant = estimatedSize.height
+                }
+            }
+        }
     }
 }
 
@@ -86,19 +131,39 @@ extension ChattingRoomViewController {
     private func configure() {
         setNib(identifier: ChatBubbleOtherUserTableViewCell.identifier, object: tableView)
         setNib(identifier: ChatBubbleUserTableViewCell.identifier, object: tableView)
-        tableView.separatorStyle = .none
+        
         setDelegate()
+        
+        tableView.separatorStyle = .none
         setNavigation()
+        setButton()
+        setTextView()
+        
         showLastChat()
     }
     
     private func setDelegate() {
         tableView.delegate = self
         tableView.dataSource = self
+        textView.delegate = self
     }
     
     private func setNavigation() {
         navigationItem.title = chatData.chatroomName
+    }
+    
+    private func setButton() {
+        sendChatButton.setImage(UIImage(systemName: "paperplane.fill"), for: .normal)
+        sendChatButton.tintColor = .gray
+    }
+    
+    private func setTextView() {
+        textView.backgroundColor = .systemGray6
+        textView.layer.cornerRadius = 15
+        textView.clipsToBounds = true
+        textView.textContainer.lineFragmentPadding = 15
+
+        resetTextViewSize()
     }
 }
 
