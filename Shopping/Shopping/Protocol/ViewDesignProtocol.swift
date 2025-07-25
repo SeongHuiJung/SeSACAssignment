0f76a039ce819b53460803aa254c770e5d730c7a
SeongHuiJung <tjdgml150237@naver.com>
2025-07-25 15:06:28 +0900

    [Fix] [Day21] 쇼핑 프로젝트 생성 및 구조 세팅

---
+++ Shopping/Shopping/Protocol/ViewDesignProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ViewDesignProtocol.swift
+//  Shopping
+//
+//  Created by 정성희 on 7/25/25.
+//
+
+import Foundation
+
+protocol ViewDesignProtocol: AnyObject {
+    func configureHierarchy()
+    func configureLayout()
+    func configureView()
+}
---
+++ Shopping/Shopping/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Shopping
+//
+//  Created by 정성희 on 7/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
