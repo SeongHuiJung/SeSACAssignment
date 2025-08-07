3928dfeb3ea1a11f7ea9207e6d42c73f8fbfc403
SeongHuiJung <tjdgml150237@naver.com>
2025-08-08 00:45:34 +0900

    [Feat] [Day30] 값 유효성을 검사하는 제네릭 함수 구현

--- MVVMBasic/MVVMBasic/Helper/ErrorManager.swift
+++ MVVMBasic/MVVMBasic/Helper/ErrorManager.swift
@@ -5,4 +5,50 @@
 //  Created by 정성희 on 8/7/25.
 //
 
-import Foundation
+import UIKit
+
+enum NumberValidationError : Error {
+    case EmptyString
+    case haveWhiteSpace
+    case isNotInt
+    case isNotDouble
+    case isNotValidType
+}
+
+enum NumberRangeError: Error {
+    case lowerTHanMinimum
+    case upperTHanMaximum
+}
+
+final class ErrorManager {
+    static let shared = ErrorManager()
+    private init() {}
+    
+    func validateUserInput<T: LosslessStringConvertible>(text: String, transferType: T.Type) throws(NumberValidationError) -> T {
+        guard !(text.isEmpty) else { throw NumberValidationError.EmptyString }
+        
+        for char in text {
+            if char == " " { throw NumberValidationError.haveWhiteSpace }
+        }
+        
+        if T.self is Int.Type {
+            guard let value = Int(text) else { throw NumberValidationError.isNotInt }
+            return value as! T
+        }
+        
+        else if T.self is Double.Type {
+            guard let value = Double(text) else { throw NumberValidationError.isNotDouble }
+            return value as! T
+        }
+        
+        else {
+            throw NumberValidationError.isNotValidType
+        }
+    }
+    
+    func validateNumberRange<T: Comparable>(value: T, min: T, max: T) throws(NumberRangeError) {
+        guard value >= min else { throw NumberRangeError.lowerTHanMinimum }
+        guard value <= max else { throw NumberRangeError.upperTHanMaximum }
+    }
+}
+
