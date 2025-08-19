842469504ce18a985632042cd86f8dbe29b097a3
SeongHuiJung <tjdgml150237@naver.com>
2025-08-19 23:22:42 +0900

    [Add] [Day37] RxSwift 프로젝트 세팅

---
+++ RxSwiftStudy/RxSwiftStudy/Features/TableView/SimpleTableViewExampleViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftStudy
+//
+//  Created by 정성희 on 8/19/25.
+//
+
+import UIKit
+
+class SimpleTableViewExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RxSwiftStudy/RxSwiftStudy/Manager/TransitionManager.swift
@@ -0,0 +1,51 @@
+//
+//  TransitionManager.swift
+//  RxSwiftStudy
+//
+//  Created by 정성희 on 8/19/25.
+//
+
+import UIKit
+
+class TransitionManager {
+    static let shared = TransitionManager()
+    private init() {}
+    
+    func getMainTabViewController() -> UITabBarController {
+        let tabBarController = UITabBarController()
+        
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBarAppearance.backgroundColor = .white
+        tabBarAppearance.stackedLayoutAppearance.selected.iconColor = .black
+        tabBarAppearance.stackedLayoutAppearance.normal.iconColor = .lightGray
+        
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        
+        let page1 = UINavigationController(rootViewController: SimpleTableViewExampleViewController())
+        let page2 = UINavigationController(rootViewController: NumbersViewController())
+        let page3 = UINavigationController(rootViewController: SimpleValidationViewController())
+        
+        page1.view.backgroundColor = .white
+        page2.view.backgroundColor = .white
+        page3.view.backgroundColor = .white
+        
+        tabBarController.setViewControllers([page1, page2, page3], animated: true)
+        
+        if let items = tabBarController.tabBar.items {
+            items[0].selectedImage = UIImage(systemName: "star.fill")
+            items[0].image = UIImage(systemName: "star")
+            
+            items[1].selectedImage = UIImage(systemName: "heart.fill")
+            items[1].image = UIImage(systemName: "heart")
+            
+            items[2].selectedImage = UIImage(systemName: "person.fill")
+            items[2].image = UIImage(systemName: "person")
+        }
+        
+        tabBarAppearance.backgroundColor = .white
+        
+        return tabBarController
+    }
+}
