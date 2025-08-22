83cf120f103e865190a9cb0aebe8020f766ee3d3
SeongHuiJung <tjdgml150237@naver.com>
2025-08-22 15:59:51 +0900

    [Add] [Day40] 다마고치 프로젝트 파일 구조 개선

---
+++ SeSACDay8HW/SeSACDay8HW/Tamagotchi/TamagotchiViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TamagotchiViewController.swift
+//  SeSACDay8HW
+//
+//  Created by 정성희 on 8/22/25.
+//
+
+import UIKit
+
+
+class TamagotchiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
